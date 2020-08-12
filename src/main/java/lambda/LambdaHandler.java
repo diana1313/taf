959f5b6ee5399a8f2b32428cc1cbe5e424f223c7
Diana Dmitriienko <diana_dmitriienko@epam.com>
2020-08-12 11:52:49 +0300

    lambda try

---
+++ src/main/java/lambda/LambdaHandler.java
@@ -0,0 +1,15 @@
+package lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+public class LambdaHandler implements RequestHandler<SNSEvent, String> {
+    @Override
+    public String handleRequest(SNSEvent event, Context context) {
+        context.getLogger().log("Received event: " + event);
+        String message = event.getRecords().get(0).getSNS().getMessage();
+        context.getLogger().log("From SNS: " + message);
+        return message;
+    }
+}
