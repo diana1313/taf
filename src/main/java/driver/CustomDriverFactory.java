f6fce0598ece0faf461c61a0547a40a8791b68d2
Diana Dmitriienko <diana_dmitriienko@epam.com>
2020-10-27 18:39:55 +0200

    finished - allure issue

--- src/main/java/driver/CustomDriverFactory.java
+++ src/main/java/driver/CustomDriverFactory.java
@@ -6,7 +6,10 @@ import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManagerException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 
 public class CustomDriverFactory {
@@ -24,9 +27,14 @@ public class CustomDriverFactory {
         DriverManagerType driverType = DriverManagerType.valueOf(PropertiesReader.getPropertyValue("webdriver.driver"));
         ChromeDriverManager.getInstance(driverType).setup();
         if (driverType.toString().equals("Google Chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            driver = new ChromeDriver(options);
         } else if (driverType.toString().equals("Mozilla Firefox")) {
             driver = new FirefoxDriver();
+//            FirefoxProfile ffprofile = new FirefoxProfile();
+//            ffprofile.setPreference("dom.webnotifications.enabled", false);
+//            driver = new FirefoxDriver(new FirefoxOptions().setProfile(ffprofile));
         } else {
             throw new WebDriverManagerException(String.format(
                     "Driver type '%s' defined in properties is not supported", driverType.toString()));
--- src/test/resources/cucumber/scenarios/PetStoreApi.feature
+++
@@ -1,9 +0,0 @@
-Feature: Pet Store API
-
-  Scenario: Verification of CRUD operations
-    When I create pet with name parameter "pit bull"
-    Then I check created pet exists and has "expected" name
-    When I update current pet with new name parameter "stafford"
-    Then I check created pet exists and has "updated" name
-    When I delete current pet
-    Then I check current pet doesn't exist
